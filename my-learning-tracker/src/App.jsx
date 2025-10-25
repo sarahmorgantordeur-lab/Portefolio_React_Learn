c7f3cc3911fa210471b31f6fc6a5a003e5bacdde
Sarah Tordeur <sarahmorgantordeur@gmail.com>
2025-10-25 21:30:30 +0200

    first commit for my learning

---
+++ my-learning-tracker/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>My Learning Tracker 📘</h1>
+      <p>Bienvenue dans ton premier projet React !</p>
+    </div>
+  );
+}
+
+export default App;
+
